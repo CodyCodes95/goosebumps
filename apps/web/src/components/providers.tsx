aa412006e55ac1ff06c349054f92b0b5e9685536
Cody Partington <codythatsme@gmail.com>
2025-08-10 15:54:09 +1000

    init with convex and auth setup

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -3,14 +3,12 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "./ui/sonner";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export default function Providers({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
@@ -18,7 +16,9 @@ export default function Providers({
       enableSystem
       disableTransitionOnChange
     >
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
       <Toaster richColors />
     </ThemeProvider>
   );
