d644ea7c6be0610a13b0c4597ac6b4724bb73514
Cody Partington <codythatsme@gmail.com>
2025-08-14 20:17:20 +1000

    evals built out

--- packages/backend/evals/trivia.eval.ts
+++ packages/backend/evals/trivia.eval.ts
@@ -1,14 +1,25 @@
 import { evalite } from "evalite";
 import { Factuality } from "./factuality";
+import { askTriviaAgent } from "../lib/agent";
 
 // Minimal example eval to bootstrap the UI per docs
 // https://www.evalite.dev/quickstart/
 export default evalite("Trivia Eval", {
   data: async (): Promise<{ input: string; expected: string }[]> => {
-    return [{ input: "Who won 2024 geoguessr world cup?", expected: "Blinky" }];
+    return [
+      { input: "Who won 2024 geoguessr world cup?", expected: "Blinky" },
+      {
+        input: "How did edward become a vampire?",
+        expected: "He was bitten by Edward",
+      },
+    ];
   },
   task: async (input) => {
-    return `${input} World!`;
+    const answers = await askTriviaAgent({
+      promptText: input,
+    });
+
+    return answers.correct;
   },
   scorers: [Factuality],
 });
